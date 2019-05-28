6c953a015c7dcdde9d4ce168f720fed77c863fa7
juanjo097 <rodriguez.rangel97@gmail.com>
2019-05-27 23:20:46 -0500

    book service CRUD operation finished and ready to testing

--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInput } from '@angular/material';
+import { BookService } from '../../shared/book.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+export interface Language {
+  name: string;
+}
 
 @Component({
   selector: 'app-add-book',
@@ -6,6 +14,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-book.component.scss']
 })
 export class AddBookComponent implements OnInit {
+
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  languageArray: Language[] = [];
+  @ViewChild('chipList') chipList;
+  @ViewChild('resetBookForm') NgForm;
+
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+
+  selectedBindingType: string;
+  bookForm: FormGroup;
+
+  BindingType: any = ['Paperback', 'Case binding', 'Perfect binding', 'Saddle stitch binding', 'Spiral binding'];
+    
 
   constructor() { }
 

43913568cc5ba2d74b03f94770f1702efb6a0ec2
juanjo097 <rodriguez.rangel97@gmail.com>
2019-05-29 23:31:23 -0500

    update and delete working just minor bugs to fix

--- src/app/components/edit-book/edit-book.component.ts
+++ src/app/components/edit-book/edit-book.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material';
+import { BookService } from '../../shared/book.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+export interface Language {
+  name: string;
+}
 
 @Component({
   selector: 'app-edit-book',
@@ -7,9 +17,111 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditBookComponent implements OnInit {
 
-  constructor() { }
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+
+  languageArray: Language[] = [];
+
+  @ViewChild('chipList') chipList;
+
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  selectedBindingType: string;
+  editBookForm: FormGroup;
+  BindingType: any = ['Paperback', 'Case binding', 'Perfect binding', 'Saddle stitch binding', 'Spiral binding'];
 
   ngOnInit() {
+    this.updateBookForm();
   }
+
+  constructor(private fb : FormBuilder,
+              private location: Location,
+              private API : BookService,
+              private actvR: ActivatedRoute,
+              private route: Router)
+              {
+
+                let id = this.actvR.snapshot.paramMap.get('id');
+                this.API.GetBook(id).valueChanges().subscribe((data)=>{
+                  console.log(data)
+                  this.languageArray = data.lenguages;
+                  this.editBookForm.setValue(data);
+                },
+                (error)=>
+                {
+                  console.log(error);
+                });
+              }
+
+  //update formm
+  updateBookForm()
+  {
+    this.editBookForm = this.fb.group({
+      book_name: ['', [Validators.required]],
+      isbn_10: ['', [Validators.required]],
+      author_name: ['', [Validators.required]],
+      publication_date: ['', [Validators.required]],
+      binding_type: ['', [Validators.required]],
+      in_stock: ['Yes'],
+      lenguages: ['']
+    });
+  }
+
+  /* Add language */
+  add(event: MatChipInputEvent): void
+  {
+
+    var input: any = event.input;
+    var value: any = event.value;
+    // Add language
+    if ((value || '').trim() && this.languageArray.length < 5)
+    {
+      this.languageArray.push({name: value.trim()});
+    }
+    // Reset the input value
+    if (input)
+    {
+      input.value = '';
+    }
+
+  }
+
+  /* Remove language */
+  remove(language: any): void {
+    const index = this.languageArray.indexOf(language);
+
+    if (index >= 0) {
+      this.languageArray.splice(index, 1);
+    }
+
+  }
+
+  /* Get errors */
+  public handleError = (controlName: string, errorName: string) =>
+  {
+    return this.editBookForm.controls[controlName].hasError(errorName);
+  }
+
+  /* Date */
+  formatDate(e) {
+    var convertDate = new Date(e.target.value).toISOString().substring(0, 10);
+    this.editBookForm.get('publication_date').setValue(convertDate, {
+      onlyself: true
+    })
+  }
+
+  goBack(){
+    this.location.back();
+  }
+
+  updateBook() {
+    var id = this.actvR.snapshot.paramMap.get('id');
+    if(window.confirm('Are you sure you wanna update?')){
+      this.API.UpdateBook(id, this.editBookForm.value);
+      this.route.navigate(['books-list']);
+    }
+  }
+
 
 }
